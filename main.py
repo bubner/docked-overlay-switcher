c659b8c5f3efd0c69c1670fe7f92a11f08740ffd
Lucas Bubner <lkbubner@proton.me>
2026-06-02 19:53:46 +0930

    Settings saving

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-import decky
+from os import getenv
 
+import decky
+from settings import SettingsManager
+
+# https://wiki.deckbrew.xyz/plugin-dev/getting-started#settingsmanager
+
+settings = SettingsManager(name="settings", settings_directory=getenv("DECKY_PLUGIN_SETTINGS_DIR"))
+settings.read()
 
 class Plugin:
     async def _main(self):
         decky.logger.info("Hello World!")
+ 
+    async def get_settings(self) -> dict[str, int | bool]:
+        """
+        Gets user defined settings from the configuration file.
+        """
+        settings.read()
+        return {
+            "handheldEnabled": settings.getSetting("handheldEnabled", True),
+            "handheldLevel": settings.getSetting("handheldLevel", 0),
+            "dockedEnabled": settings.getSetting("dockedEnabled", True),
+            "dockedLevel": settings.getSetting("dockedLevel", 0)
+        }
+    
+    async def set_settings(self, new_settings: dict[str, int | bool]):
+        """
+        Updates and commits new user settings.
+        """
+        for key, value in new_settings.items():
+            settings.setSetting(key, value)
+        settings.commit()
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,42 @@
-import { definePlugin } from "@decky/api";
+import { callable, definePlugin } from "@decky/api";
 import { PanelSection, SliderField, staticClasses, ToggleField } from "@decky/ui";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaTv } from "react-icons/fa";
 
-function PerformanceSlider({
-    initialValue,
-    onChange,
-}: {
-    initialValue: number;
-    onChange: (v: number) => void;
-}) {
-    const [enabled, setEnabled] = useState(false);
-    const [value, setValue] = useState(initialValue);
+type Settings = {
+    handheldEnabled: boolean;
+    handheldLevel: number;
+    dockedEnabled: boolean;
+    dockedLevel: number;
+};
 
+const getSettings = callable<[], Settings>("get_settings");
+
+const setSettings = callable<[new_settings: Settings], void>("set_settings");
+
+function PerformanceSlider({
+    enabled,
+    level,
+    onEnabledChange,
+    onLevelChange,
+}: {
+    enabled: boolean;
+    level: number;
+    onEnabledChange: (v: boolean) => void;
+    onLevelChange: (v: number) => void;
+}) {
     return (
         <>
             <ToggleField
                 label="Apply level on change"
                 description="Whether the overlay should be updated when changing to this mode"
                 checked={enabled}
-                onChange={(e) => setEnabled(e)}
+                onChange={(e) => onEnabledChange(e)}
             />
             <SliderField
                 label="Performance Overlay Level"
                 disabled={!enabled}
-                value={value}
+                value={level}
                 min={0}
                 max={4}
                 step={1}
@@ -52,31 +64,46 @@ function PerformanceSlider({
                         label: "4",
                     },
                 ]}
-                onChange={(v) => {
-                    setValue(v);
-                    onChange(v);
-                }}
+                onChange={(v) => onLevelChange(v)}
             />
         </>
     );
 }
 
-function onHandheldChange(level: number) {
-  // todo
-}
-
-function onDockedChange(level: number) {
-  // todo
-}
-
 function Content() {
+    const [settings, setUISettings] = useState<Settings | null>(null);
+
+    useEffect(() => {
+        getSettings().then((settings) => setUISettings(settings));
+    }, []);
+
+    async function dispatch(diff: Partial<Settings>) {
+        const newSettings = { ...settings, ...diff } as Settings;
+        await setSettings(newSettings); // config file
+        setUISettings(newSettings); // local ui
+    }
+
+    if (!settings) {
+        return "Loading...";
+    }
+
     return (
         <>
             <PanelSection title="Handheld Mode">
-                <PerformanceSlider initialValue={0} onChange={onHandheldChange} />
+                <PerformanceSlider
+                    level={settings.handheldLevel}
+                    enabled={settings.handheldEnabled}
+                    onEnabledChange={(e) => dispatch({ handheldEnabled: e })}
+                    onLevelChange={(v) => dispatch({ handheldLevel: v })}
+                />
             </PanelSection>
             <PanelSection title="Docked Mode">
-                <PerformanceSlider initialValue={0} onChange={onDockedChange} />
+                <PerformanceSlider
+                    level={settings.dockedLevel}
+                    enabled={settings.dockedEnabled}
+                    onEnabledChange={(e) => dispatch({ dockedEnabled: e })}
+                    onLevelChange={(v) => dispatch({ dockedLevel: v })}
+                />
             </PanelSection>
         </>
     );
