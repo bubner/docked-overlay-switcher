6357b22e1c2a4914b71af33a118c472d6c5baf14
Lucas Bubner <lkbubner@proton.me>
2026-06-02 18:43:44 +0930

    Initial frontend implementation and clean out template code

--- src/index.tsx
+++ src/index.tsx
@@ -1,115 +1,92 @@
-import {
-  ButtonItem,
-  PanelSection,
-  PanelSectionRow,
-  Navigation,
-  staticClasses
-} from "@decky/ui";
-import {
-  addEventListener,
-  removeEventListener,
-  callable,
-  definePlugin,
-  toaster,
-  // routerHook
-} from "@decky/api"
+import { definePlugin } from "@decky/api";
+import { PanelSection, SliderField, staticClasses, ToggleField } from "@decky/ui";
 import { useState } from "react";
-import { FaShip } from "react-icons/fa";
+import { FaTv } from "react-icons/fa";
 
-// import logo from "../assets/logo.png";
+function PerformanceSlider({
+    initialValue,
+    onChange,
+}: {
+    initialValue: number;
+    onChange: (v: number) => void;
+}) {
+    const [enabled, setEnabled] = useState(false);
+    const [value, setValue] = useState(initialValue);
 
-// This function calls the python function "add", which takes in two numbers and returns their sum (as a number)
-// Note the type annotations:
-//  the first one: [first: number, second: number] is for the arguments
-//  the second one: number is for the return value
-const add = callable<[first: number, second: number], number>("add");
+    return (
+        <>
+            <ToggleField
+                label="Apply level on change"
+                description="Whether the overlay should be updated when changing to this mode"
+                checked={enabled}
+                onChange={(e) => setEnabled(e)}
+            />
+            <SliderField
+                label="Performance Overlay Level"
+                disabled={!enabled}
+                value={value}
+                min={0}
+                max={4}
+                step={1}
+                notchCount={5}
+                notchTicksVisible={false}
+                notchLabels={[
+                    {
+                        notchIndex: 0,
+                        label: "OFF",
+                    },
+                    {
+                        notchIndex: 1,
+                        label: "1",
+                    },
+                    {
+                        notchIndex: 2,
+                        label: "2",
+                    },
+                    {
+                        notchIndex: 3,
+                        label: "3",
+                    },
+                    {
+                        notchIndex: 4,
+                        label: "4",
+                    },
+                ]}
+                onChange={(v) => {
+                    setValue(v);
+                    onChange(v);
+                }}
+            />
+        </>
+    );
+}
 
-// This function calls the python function "start_timer", which takes in no arguments and returns nothing.
-// It starts a (python) timer which eventually emits the event 'timer_event'
-const startTimer = callable<[], void>("start_timer");
+function onHandheldChange(level: number) {
+  // todo
+}
+
+function onDockedChange(level: number) {
+  // todo
+}
 
 function Content() {
-  const [result, setResult] = useState<number | undefined>();
-
-  const onClick = async () => {
-    const result = await add(Math.random(), Math.random());
-    setResult(result);
-  };
-
-  return (
-    <PanelSection title="Panel Section">
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={onClick}
-        >
-          {result ?? "Add two numbers via Python"}
-        </ButtonItem>
-      </PanelSectionRow>
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => startTimer()}
-        >
-          {"Start Python timer"}
-        </ButtonItem>
-      </PanelSectionRow>
-
-      {/* <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
-      </PanelSectionRow> */}
-
-      {/*<PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => {
-            Navigation.Navigate("/decky-plugin-test");
-            Navigation.CloseSideMenus();
-          }}
-        >
-          Router
-        </ButtonItem>
-      </PanelSectionRow>*/}
-    </PanelSection>
-  );
-};
+    return (
+        <>
+            <PanelSection title="Handheld Mode">
+                <PerformanceSlider initialValue={0} onChange={onHandheldChange} />
+            </PanelSection>
+            <PanelSection title="Docked Mode">
+                <PerformanceSlider initialValue={0} onChange={onDockedChange} />
+            </PanelSection>
+        </>
+    );
+}
 
 export default definePlugin(() => {
-  console.log("Template plugin initializing, this is called once on frontend startup")
-
-  // serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-  //   exact: true,
-  // });
-
-  // Add an event listener to the "timer_event" event from the backend
-  const listener = addEventListener<[
-    test1: string,
-    test2: boolean,
-    test3: number
-  ]>("timer_event", (test1, test2, test3) => {
-    console.log("Template got timer_event with:", test1, test2, test3)
-    toaster.toast({
-      title: "template got timer_event",
-      body: `${test1}, ${test2}, ${test3}`
-    });
-  });
-
-  return {
-    // The name shown in various decky menus
-    name: "Test Plugin",
-    // The element displayed at the top of your plugin's menu
-    titleView: <div className={staticClasses.Title}>Decky Example Plugin</div>,
-    // The content of your plugin's menu
-    content: <Content />,
-    // The icon displayed in the plugin list
-    icon: <FaShip />,
-    // The function triggered when your plugin unloads
-    onDismount() {
-      console.log("Unloading")
-      removeEventListener("timer_event", listener);
-      // serverApi.routerHook.removeRoute("/decky-plugin-test");
-    },
-  };
+    return {
+        name: "Docked Overlay Switcher",
+        titleView: <div className={staticClasses.Title}>Overlay Switcher</div>,
+        content: <Content />,
+        icon: <FaTv />,
+    };
 });
